5fb873e77455e74d50d7b0d76c291059ff01ab78
Ariel Tubaltsev <tubaltzev@gmail.com>
2013-12-23 15:46:31 -0800

    repo make/clean

--- lib/bs/config.rb
+++ lib/bs/config.rb
@@ -3,9 +3,28 @@ module BS
 
     @config = nil
 
+    CONF_FILE = '.bs/config'
+
+    init_config = {
+      'task_path' => '.bs/tasks'
+    }
+
     def self.get
-      @config = YAML::load_file('.bs/config') unless @config
+      @config = YAML::load_file(CONF_FILE) unless @config
       return @config
+    end
+
+    def init
+      File.open(CONF_FILE, 'w') {|f| f.write init_config.to_yaml }
+    end
+
+    def clean
+      system("./bs")
+    end
+
+    def make
+      system("mkdir -p ./bs")
+      init
     end
 
   end
