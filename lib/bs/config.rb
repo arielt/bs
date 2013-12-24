4dd7e1af2b57fabe9a40356c7893968442fbf564
Ariel <tubaltzev@gmail.com>
2013-12-23 16:27:07 -0800

    repo check

--- lib/bs/config.rb
+++ lib/bs/config.rb
@@ -3,11 +3,21 @@ module BS
 
     @config = nil
 
+    CONF_DIR  = '.bs'
     CONF_FILE = '.bs/config'
+    TASK_DIR  = '.bs/tasks'
 
     @init_config = {
       'task_path' => '.bs/tasks'
     }
+
+   def self.check()
+     unless Dir.exists? CONF_DIR
+       puts "A Binary Score environment is required to run this command.
+Run `bs make` to set one up."
+       exit(1)
+     end
+   end
 
     def self.get
       @config = YAML::load_file(CONF_FILE) unless @config
@@ -19,12 +29,12 @@ module BS
     end
 
     def self.clean(params)
-      system("rm -rf .bs")
+      system("rm -rf #{CONF_DIR}")
     end
 
     def self.make(params)
-      system("mkdir -p .bs")
-      system("mkdir -p .bs/tasks")
+      system("mkdir -p #{CONF_DIR}")
+      system("mkdir -p #{TASK_DIR}")
       init
     end
 
--- lib/bs/task.rb
+++ lib/bs/task.rb
@@ -1,6 +1,7 @@
 module BS
   module Task
     def self.list(params)
+      BS::Config.check
       puts "Task list:"
       puts "-------------------------------------"
       system("ls #{BS::Config.get['task_path']}")
@@ -8,10 +9,12 @@ module BS
     end
 
     def self.add(params)
+      BS::Config.check
       system("cd  #{BS::Config.get['task_path']} && git clone #{params[0]}")
     end
 
     def self.del(params)
+      BS::Config.check
       system("rm -rf #{BS::Config.get['task_path']}/#{params[0]}")
     end
   end
