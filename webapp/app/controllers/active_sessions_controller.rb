936a8f3f2c6ccc85a1d7064a551d5d21d2fe0a9b
void <tubaltzev@gmail.com>
2014-01-06 17:26:14 -0800

    active sessions view

--- webapp/app/controllers/active_sessions_controller.rb
+++ webapp/app/controllers/active_sessions_controller.rb
@@ -13,5 +13,59 @@ class ActiveSessionsController < ApplicationController
     @is_accepted = @session_config["is_accepted"]
   end
 
+
+  # Ajax POST call
+  def create
+    # lock the session
+    return respond_to {|format| format.json {render :json => {status: STATUS_BUSY}}} unless BS::Session.lock()
+
+    # sumbit solution
+    if params["submit"] == "TRUE" then
+      @current_session.is_active     = FALSE;
+      @current_session.finished_at   = Time.now;
+      @current_session.forced_finish = FALSE;
+      @current_session.save
+      @current_session.unlock()
+      return respond_to {|format| format.json {render :json => {status: STATUS_OK}}} 
+    end
+    
+    # calculate digest
+    request_digest = Digest::MD5.hexdigest(params["check"].to_s)
+
+    # return saved state if there is no change in code
+    if request_digest == @current_session.verified_digest
+      parse_message()
+      
+      # if there was a timeout - allow to verify once again
+      if @current_session.verified_message.include?("[--bs--]:Oops... We've been waiting for too long")
+        @current_session.verified_digest = nil
+      end
+
+      @current_session.response = {
+        status:   @current_session.verified_status, 
+        issues:   JSON.parse(@current_session.verified_issues),
+        solution: @current_session.vr_solution
+      }.to_json
+      @current_session.save
+ 
+      @current_session.unlock()
+      return respond_to {|format| format.json {render :json => @current_session.response}}
+    end
+
+    # start the verification
+    params_hash = {
+      user_test_id: @current_session.user_test_id,
+      solution: params["check"],
+      digest: request_digest,
+      session_id: @current_session.id
+    }
+    Resque.enqueue(Sandbox::First, params_hash)
+
+    @current_session.vr_solution = params["check"];
+    @current_session.save!;
+
+    respond_to {|format| format.json { render :json => {status: STATUS_BUSY}}}
+  end
+
 end
 
